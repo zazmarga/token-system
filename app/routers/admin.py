1a11d0c7fe730097ced8f68e7aafd4af4ea8be26
Marharyta Zazulia <zazmargo@gmail.com>
2026-01-11 00:04:45 -0300

    add endpoint to get usage statistics: previous version (admin API)

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1,12 +1,20 @@
-from fastapi import APIRouter, Depends, status, HTTPException
-from sqlalchemy import select, func
+from datetime import date, datetime, timezone, time
+from typing import Optional
+
+from fastapi import APIRouter, Depends, status, HTTPException, Query
+from sqlalchemy import select, func, case
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.dependencies import access_admin, get_db
+from app.models import Credits, Transaction, TransactionType
 from app.models.user import User
 from app.models.settings import Settings
 from app.models.subscription import SubscriptionPlan, Subscription
 from app.schemas.admin import ExchangeRateResponse, ExchangeRateUpdate
+from app.schemas.base import (
+    StatisticsResponse, StatisticsPeriod, StatisticsPlans,
+    StatisticsCredits, StatisticsTransactions
+)
 from app.schemas.subscription import (
     SubscriptionPlanResponse,
     SubscriptionPlanCreate,
@@ -409,4 +417,126 @@ async def update_purchase_rate(
         old_purchase_rate=old_purchase_rate,
         new_purchase_rate=purchase_rate,
         updated_at=plan.updated_at
+    )
+
+
+@admin_router.get(
+    "/statistics",
+    dependencies=[Depends(access_admin)],
+    summary="Отримання статистики використання",
+    description="Доступ лише для адміністратора. Headers: X-Admin-Token",
+    response_model=StatisticsResponse,
+    status_code=status.HTTP_200_OK,
+    responses={
+        403: {
+            "description": "Forbidden.",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Invalid admin token."}
+                },
+            },
+        },
+        500: {
+            "description": "Internal Server Error.",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Internal Server Error."}
+                }
+            },
+        },
+    },
+)
+async def get_usage_statistics(
+    start_date: date = Query(..., description="Start date (ISO 8601, e.g. 2026-01-01)"),
+    end_date: date = Query(..., description="End date (ISO 8601, e.g. 2026-01-31)"),
+    tier: Optional[str] = Query(None, description="Tier: optional"),
+    db: AsyncSession = Depends(get_db)
+):
+    # формування дати-часу з дати
+    start: datetime = datetime.combine(start_date, time(0, 0, 0), tzinfo=timezone.utc)
+    end: datetime = datetime.combine(end_date, time(23, 59, 59), tzinfo=timezone.utc)
+
+    # dict з кількістю users у кожний підписці
+    # загальна кількість користувачів які мають підписку
+    stmt = (
+        select(SubscriptionPlan.tier, func.count(User.id))
+        .join(Subscription, Subscription.user_id == User.id)
+        .join(SubscriptionPlan, Subscription.plan_id == SubscriptionPlan.tier)
+        .group_by(SubscriptionPlan.tier)
+    )
+
+    result = await db.execute(stmt)
+    rows = result.all()
+
+    subscriptions = {tier: count for tier, count in rows}
+    total_users = sum(subscriptions.values())
+    if tier:
+        if tier in subscriptions:
+            subscriptions = {tier: subscriptions[tier]}
+            total_users = subscriptions[tier]
+        else:
+            subscriptions = {}
+            total_users = 0
+
+    # статистика по кредитам
+    stmt = (
+        select(
+            func.sum(Credits.total_earned).label("total_earned"),
+            func.sum(Credits.total_spent).label("total_spent"),
+        )
+        .select_from(User)
+        .join(Subscription, Subscription.user_id == User.id)
+        .join(SubscriptionPlan, Subscription.plan_id == SubscriptionPlan.tier)
+    )
+
+    if tier:
+        stmt = stmt.where(SubscriptionPlan.tier == tier)
+
+    result = await db.execute(stmt)
+    row = result.one()
+
+    total_earned = row.total_earned or 0
+    total_spent = row.total_spent or 0
+    current_balance = total_earned - total_spent
+
+    # статистика кількості транзакцій
+    stmt = (
+        select(
+            func.count(Transaction.id).label("total"),
+            func.sum(case((Transaction.type == TransactionType.CHARGE, 1), else_=0)).label("charges"),
+            func.sum(case((Transaction.type == TransactionType.ADD, 1), else_=0)).label("additions"),
+        )
+        .join(Subscription, Subscription.user_id == Transaction.user_id)
+        .join(SubscriptionPlan, Subscription.plan_id == SubscriptionPlan.tier)
+        .where(Transaction.created_at >= start_date)
+        .where(Transaction.created_at < end_date)
+    )
+
+    if tier:
+        stmt = stmt.where(SubscriptionPlan.tier == tier)
+
+    result = await db.execute(stmt)
+    row = result.one()
+
+    total = row.total or 0
+    charges = row.charges or 0
+    additions = row.additions or 0
+
+    return StatisticsResponse(
+        period=StatisticsPeriod(
+            start=start,
+            end=end
+        ),
+        total_users=total_users,
+        subscriptions=StatisticsPlans(subscriptions=subscriptions),
+        credits=StatisticsCredits(
+            total_earned=total_earned,
+            total_spent=total_spent,
+            current_balance=current_balance
+        ),
+        transactions=StatisticsTransactions(
+            total=total,
+            charges=charges,
+            additions=additions
+        )
     )
--- app/schemas/base.py
+++ app/schemas/base.py
@@ -1,7 +1,40 @@
+from datetime import datetime
+from typing import Dict
+
 from pydantic import BaseModel
 
 
-# circular import problem
+# Statistics Admin
+class StatisticsPeriod(BaseModel):
+	start: datetime
+	end: datetime
+
+
+class StatisticsPlans(BaseModel):
+	subscriptions: Dict[str, int]
+
+
+class StatisticsCredits(BaseModel):
+	total_earned: int
+	total_spent: int
+	current_balance: int
+
+
+class StatisticsTransactions(BaseModel):
+	total: int
+	charges: int
+	additions: int
+
+
+class StatisticsResponse(BaseModel):
+	period: StatisticsPeriod
+	total_users: int
+	subscriptions: StatisticsPlans
+	credits: StatisticsCredits
+	transactions: StatisticsTransactions
+
+
+# problem: circular import
 class UserCreditsBase(BaseModel):
 	balance: int
 	total_earned: int
